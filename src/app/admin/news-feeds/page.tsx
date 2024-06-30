9136664342589d33db45ed44ad7bf6115959bc27
dinhkhai0201 <nguyendinhkhai2111997@gmail.com>
2024-06-30 14:05:43 +0700

    sample files

--- src/app/admin/news-feeds/page.tsx
+++ src/app/admin/news-feeds/page.tsx
@@ -5,39 +5,39 @@ import NewsFeedItem from 'components/admin/default/NewsFeedItem';
 const NEWSFEEDS = [
   {
     id: 1,
-    uri: 'https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf',
-    type: 'pdf',
-    description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
-  },
-  {
-    id: 2,
-    uri: 'https://pdfobject.com/pdf/sample.pdf',
+    uri: 'https://hoiana-testing.vercel.app/img/example/pdf.pdf',
     type: 'pdf',
     description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
   },
   {
     id: 3,
-    uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Blank_page_intentionally_end_of_book.jpg/640px-Blank_page_intentionally_end_of_book.jpg',
+    uri: 'https://hoiana-testing.vercel.app/img/example/1.webp',
     type: 'jpg',
     description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
   },
   {
     id: 4,
-    uri: 'https://res.cloudinary.com/cloudinary-marketing/images/c_fill,w_700/f_auto,q_auto/v1649720751/Web_Assets/blog/Mario_1/Mario_1-gif?_i=AA',
+    uri: 'https://hoiana-testing.vercel.app/img/example/gif.gif',
     type: 'gif',
-    description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
+    description: "gif"
   },
   {
     id: 5,
-    uri: 'https://freetestdata.com/wp-content/uploads/2022/02/Free_Test_Data_2MB_MP4.mp4',
+    uri: 'https://hoiana-testing.vercel.app/img/example/mp4.mp4',
     type: 'mp4',
-    description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
+    description: "mp4"
   },
   {
     id: 6,
-    uri: 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_300KB_XLSX.xlsx',
+    uri:'https://hoiana-testing.vercel.app/img/example/xlsx.xlsx',
     type: 'xlsx',
-    description: "Hoiana Resort & Golf combines stylish accommodation, world-class golf and exhilarating entertainment in Vietnam's first and finest integrated resort."
+    description: "xlsx"
+  },
+  {
+    id: 6,
+    uri:'https://hoiana-testing.vercel.app/img/example/sample.doc',
+    type: 'doc',
+    description: "doc"
   },
 ];
 
